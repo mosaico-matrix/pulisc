f7d4348b644d079829b2be6433fab2ff363f981e
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-21 18:00:50 +0200

    Reverted to simple until update

--- widget.py
+++ widget.py
@@ -4,60 +4,7 @@ from datetime import datetime
 
 # Load roommates from config
 roommates = config["roommates"]
-
-# Files to store the current index and last date checked
-INDEX_FILE = "current_index.txt"
-DATE_FILE = "last_date.txt"
-
-def load_index():
-    """Load the current index from the file."""
-    try:
-        if os.path.exists(INDEX_FILE):
-            with open(INDEX_FILE, "r") as file:
-                return int(file.read().strip())
-    except Exception as e:
-        return 0
-    return 0
-
-def save_index(index):
-    """Save the current index to the file."""
-    with open(INDEX_FILE, "w") as file:
-        file.write(str(index))
-
-def load_date():
-    """Load the last date checked from the file."""
-    if os.path.exists(DATE_FILE):
-        with open(DATE_FILE, "r") as file:
-            return file.read().strip()
-    return None
-
-def save_date(date_str):
-    """Save the current date to the file."""
-    with open(DATE_FILE, "w") as file:
-        file.write(date_str)
-
-# Load the current index and last date checked
-current_index = load_index()
-last_date = load_date()
-
-# Get today's date
-today_date = datetime.now().strftime("%Y-%m-%d")
-print("NOGG")
-today = datetime.strptime(today_date, "%Y-%m-%d")
-print("NOGG2")
-
-# Calculate the number of days passed since the last check
-if last_date:
-    last_checked = datetime.strptime(last_date, "%Y-%m-%d")
-    days_passed = (today - last_checked).days
-else:
-    days_passed = 0
-
-# Update the index based on the number of days passed
-if days_passed > 0:
-    current_index = (current_index + days_passed) % len(roommates)
-    save_index(current_index)
-    save_date(today_date)
+current_index=0
 
 # Select the current roommate
 current_roommate = roommates[current_index]
@@ -89,3 +36,59 @@ def loop():
         flipped = True
         vacuum.flipHorizontally()
         vacuum.animateTo(80, 30, 4000)
+
+
+# # Files to store the current index and last date checked
+# INDEX_FILE = "current_index.txt"
+# DATE_FILE = "last_date.txt"
+# 
+# def load_index():
+#     """Load the current index from the file."""
+#     try:
+#         if os.path.exists(INDEX_FILE):
+#             with open(INDEX_FILE, "r") as file:
+#                 return int(file.read().strip())
+#     except Exception as e:
+#         return 0
+#     return 0
+# 
+# def save_index(index):
+#     """Save the current index to the file."""
+#     with open(INDEX_FILE, "w") as file:
+#         file.write(str(index))
+# 
+# def load_date():
+#     """Load the last date checked from the file."""
+#     if os.path.exists(DATE_FILE):
+#         with open(DATE_FILE, "r") as file:
+#             return file.read().strip()
+#     return None
+# 
+# def save_date(date_str):
+#     """Save the current date to the file."""
+#     with open(DATE_FILE, "w") as file:
+#         file.write(date_str)
+# 
+# # Load the current index and last date checked
+# current_index = load_index()
+# last_date = load_date()
+# 
+# # Get today's date
+# today_date = datetime.now().strftime("%Y-%m-%d")
+# print("NOGG")
+# today = datetime.strptime(today_date, "%Y-%m-%d")
+# print("NOGG2")
+# 
+# # Calculate the number of days passed since the last check
+# if last_date:
+#     last_checked = datetime.strptime(last_date, "%Y-%m-%d")
+#     days_passed = (today - last_checked).days
+# else:
+#     days_passed = 0
+# 
+# # Update the index based on the number of days passed
+# if days_passed > 0:
+#     current_index = (current_index + days_passed) % len(roommates)
+#     save_index(current_index)
+#     save_date(today_date)
+        
